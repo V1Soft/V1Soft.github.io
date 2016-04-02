647595ffe5410e7021c5f6e7432b928949721b26
Connor E. Haight <connor.haight@gmail.com>
2016-04-02 11:10:50 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+function dropdown(id) {document.getElementById(id).classList.toggle("show");}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {openDropdown.classList.remove('show');}
+    }
+ }
+}
